254c3e5246aacc3aa44f59b87de2fee3000fbdd6
RaphGL <raphfl.dev@gmail.com>
2022-08-03 16:35:25 +0100

    made linked list generic

--- data_structures/queue.h
+++ data_structures/queue.h
@@ -1,3 +1,5 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -63,3 +65,4 @@
             return val;                                      \
         }                                                    \
     }
+#endif
