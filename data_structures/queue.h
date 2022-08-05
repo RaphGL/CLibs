aa5ddeebe51f9c23de88652482882343778ebf44
RaphGL <rafalopes@live.com>
2022-08-05 10:06:33 +0100

    added  queue_size and queue_free and some minor fixes

--- data_structures/queue.h
+++ data_structures/queue.h
@@ -25,11 +25,12 @@
         return new;                                          \
     }                                                        \
                                                              \
-    void *queue_node_##T(T val)                              \
+    QueueNode_##T *queue_node_##T(T val)                     \
     {                                                        \
         QueueNode_##T *new = malloc(sizeof(*new));           \
         new->item = val;                                     \
         new->next = NULL;                                    \
+        return new;                                          \
     }                                                        \
                                                              \
     int queue_enqueue_##T(Queue_##T *q, T val)               \
@@ -45,24 +46,45 @@
         return -1;                                           \
     }                                                        \
                                                              \
-    int queue_dequeue_##T(Queue_##T *q)                      \
+    T queue_dequeue_##T(Queue_##T *q)                        \
     {                                                        \
-        if (q->size == 1)                                    \
-        {                                                    \
-            free(q->head);                                   \
-            q->head = NULL;                                  \
-            q->size--;                                       \
-        }                                                    \
         if (q->size > 1)                                     \
         {                                                    \
             QueueNode_##T *tmp = q->head;                    \
             for (; tmp->next->next != NULL; tmp = tmp->next) \
                 ;                                            \
-            int val = tmp->next->item;                       \
+            T val = tmp->next->item;                         \
             free(tmp->next);                                 \
             tmp->next = NULL;                                \
             q->size--;                                       \
             return val;                                      \
         }                                                    \
+        /* q->size == 1 */                                   \
+        T val = q->head->item;                               \
+        free(q->head);                                       \
+        q->head = NULL;                                      \
+        q->size--;                                           \
+        return val;                                          \
+    }                                                        \
+                                                             \
+    int queue_size_##T(Queue_##T *q)                         \
+    {                                                        \
+        QueueNode_##T *tmp = q->head;                        \
+        int size = 0;                                        \
+        for (; tmp != NULL; tmp = tmp->next)                 \
+        {                                                    \
+            size++;                                          \
+        }                                                    \
+        return size;                                         \
+    }                                                        \
+                                                             \
+    void queue_free_##T(Queue_##T *q)                        \
+    {                                                        \
+        int size = queue_size_##T(q);                        \
+        for (int i = 0; i < size; i++)                       \
+        {                                                    \
+            queue_dequeue_##T(q);                            \
+        }                                                    \
+        free(q);                                             \
     }
 #endif
