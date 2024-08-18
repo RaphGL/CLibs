f98f918e102184e25ab4e5e9d1f5ee7a6dc1d9ca
RaphGL <raphfl.dev@gmail.com>
2024-08-18 15:40:49 -0100

    migrated to cmake

--- Makefile
+++
@@ -1,8 +0,0 @@
-.phony: test
-
-test: 
-	$(MAKE) -C bstr test 1> /dev/null
-	$(MAKE) -C flag test 1> /dev/null
-	$(MAKE) -C vec test 1> /dev/null
-	@echo "All tests passed"
-	
--- flag/flag.h
+++ flag/flag.h
@@ -11,18 +11,26 @@ typedef struct flag_parser {
   struct vec_vector *remaining;
 } flag_Parser;
 
+// Initializes parser.
+// Parser should later be freed with `flag_free`.
 flag_Parser flag_new(int argc, char *argv[]);
 void flag_free(flag_Parser *p);
 
+// Parses flags that have been setup by the `flag_<type>` functions.
+// This function should only be called after the setup has done. 
 void flag_parse(flag_Parser *p);
+// Returns true if flags have been parsed.
 bool flag_parsed(flag_Parser *p);
+// Returns the number of flags that have been configured.
 size_t flag_nflags(flag_Parser *p);
+// Prints all the flags that have been setup for the parser.
 void flag_print_flags(flag_Parser *p);
 
 
+// Returns the number of remaining args after flags have been handled.
 size_t flag_nargs(flag_Parser *p);
+// Retrieves remaining args after flags have been handled by index starting from 0.
 char *flag_arg(flag_Parser *p, size_t idx);
-
 
 long *flag_long(flag_Parser *p, char *name, long default_val, char *usage);
 float *flag_float(flag_Parser *p, char *name, float default_val, char *usage);
