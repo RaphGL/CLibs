0803109197f76dea992f348b3708758c27e98580
RaphGL <raphfl.dev@gmail.com>
2025-11-14 22:17:17 -0100

    updated vector to use saner generics

--- vec/maintest.c
+++ vec/maintest.c
@@ -1,107 +1,115 @@
+#define VEC_ITEM_TYPE int
 #include "vector.h"
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
 void test_int(void) {
-  int arr[] = {1, 2, 3, 4, 5};
-  vec_Vector *int_vec =
-      vec_from(arr, sizeof(arr) / sizeof(arr[0]), sizeof(arr[0]));
-  vec_printf("%d", int_vec);
-  assert(vec_len(int_vec) == 5);
-  vec_free(int_vec);
+   int arr[] = { 1, 2, 3, 4, 5 };
+   vec_Vector_int *int_vec = vec_from_int(arr, sizeof(arr) / sizeof(arr[0]), sizeof(arr[0]));
+   vec_printf_int("%d", int_vec);
+   assert(vec_len_int(int_vec) == 5);
+   vec_free_int(int_vec);
 
-  vec_Vector *vec = vec_new(sizeof(int));
-  assert(vec_len(vec) == 0);
-  assert(vec_capacity(vec) == 0);
+   vec_Vector_int *vec = vec_new_int();
+   assert(vec_len_int(vec) == 0);
+   assert(vec_capacity_int(vec) == 0);
 
-  int val = 0;
-  vec_push(vec, &val);
-  val = 1;
-  vec_push(vec, &val);
-  val = 2;
-  vec_push(vec, &val);
-  val = 3;
-  vec_push(vec, &val);
-  val = 4;
-  vec_push(vec, &val);
-  assert(vec_len(vec) == 5);
+   int val = 0;
+   vec_push_int(vec, val);
+   val = 1;
+   vec_push_int(vec, val);
+   val = 2;
+   vec_push_int(vec, val);
+   val = 3;
+   vec_push_int(vec, val);
+   val = 4;
+   vec_push_int(vec, val);
+   assert(vec_len_int(vec) == 5);
 
-  vec_pop(vec, &val);
-  vec_pop(vec, &val);
-  assert(vec_len(vec) == 3);
-  assert(val == 3);
-  assert(vec_capacity(vec) > 0);
+   vec_pop_int(vec, &val);
+   vec_pop_int(vec, &val);
+   assert(vec_len_int(vec) == 3);
+   assert(val == 3);
+   assert(vec_capacity_int(vec) > 0);
 
-  val = 69;
-  vec_insert(vec, 2, &val);
-  val = 23432;
-  vec_remove(vec, 2, &val);
-  assert(val == 69);
+   val = 69;
+   vec_insert_int(vec, 2, val);
+   val = 23432;
+   vec_remove_int(vec, 2, &val);
+   assert(val == 69);
 
-  vec_printf("%d", vec);
+   vec_printf_int("%d", vec);
 
-  vec_Vector *vec2 = vec_new(sizeof(int));
-  val = 0;
-  vec_push(vec2, &val);
-  val = 1;
-  vec_push(vec2, &val);
-  val = 2;
-  vec_push(vec2, &val);
-  val = 3;
-  vec_push(vec2, &val);
-  size_t len = vec_len(vec);
-  vec_append(vec, vec2);
-  assert(vec_len(vec) == vec_len(vec2) + len);
+   vec_Vector_int *vec2 = vec_new_int();
+   val = 0;
+   vec_push_int(vec2, val);
+   val = 1;
+   vec_push_int(vec2, val);
+   val = 2;
+   vec_push_int(vec2, val);
+   val = 3;
+   vec_push_int(vec2, val);
+   size_t len = vec_len_int(vec);
+   vec_append_int(vec, vec2);
+   assert(vec_len_int(vec) == vec_len_int(vec2) + len);
 
-  vec_free(vec2);
-  vec_free(vec);
-  puts("Primitive type test passed");
+   vec_free_int(vec2);
+   vec_free_int(vec);
+   puts("Primitive type test passed");
 }
 
+typedef struct {
+   float gravity;
+   int year;
+   char *name;
+} TestStruct;
+
+#define VEC_ITEM_TYPE TestStruct
+#define VEC_SUFFIX teststruct
+#include "vector.h"
+
 void test_struct(void) {
-  typedef struct {
-    float gravity;
-    int year;
-    char *name;
-  } TestStruct;
 
-  vec_Vector *vec = vec_new(sizeof(TestStruct));
-  assert(vec_is_empty(vec) == true);
-  assert(vec_len(vec) == 0);
-  assert(vec_capacity(vec) == 0);
+   vec_Vector_teststruct *vec = vec_new_teststruct();
+   assert(vec_is_empty_teststruct(vec) == true);
+   assert(vec_len_teststruct(vec) == 0);
+   assert(vec_capacity_teststruct(vec) == 0);
 
-  vec_push(vec, &(TestStruct){
-                    .name = "vector",
-                    .gravity = 9.8,
-                    .year = 2222,
-                });
+   vec_push_teststruct(vec,
+       (TestStruct) {
+           .name = "vector",
+           .gravity = 9.8,
+           .year = 2222,
+       });
 
-  assert(vec_capacity(vec) == sizeof(TestStruct));
-  vec_push(vec, &(TestStruct){
-                    .name = "vector",
-                    .gravity = 9.8,
-                    .year = 2222,
-                });
-  vec_push(vec, &(TestStruct){
-                    .name = "vector",
-                    .gravity = 9.8,
-                    .year = 2222,
-                });
-  assert(vec_len(vec) == 3);
+   assert(vec_capacity_teststruct(vec) == sizeof(TestStruct));
+   vec_push_teststruct(vec,
+       (TestStruct) {
+           .name = "vector",
+           .gravity = 9.8,
+           .year = 2222,
+       });
+   vec_push_teststruct(vec,
+       (TestStruct) {
+           .name = "vector",
+           .gravity = 9.8,
+           .year = 2222,
+       });
+   assert(vec_len_teststruct(vec) == 3);
 
-  TestStruct val;
-  vec_pop(vec, &val);
-  assert(vec_len(vec) == 2);
-  assert(val.year == 2222);
-  assert(strcmp(val.name, "vector") == 0);
+   TestStruct val;
+   vec_pop_teststruct(vec, &val);
+   assert(vec_len_teststruct(vec) == 2);
+   assert(val.year == 2222);
+   assert(strcmp(val.name, "vector") == 0);
 
-  vec_free(vec);
-  puts("Struct test passed");
+   vec_free_teststruct(vec);
+   puts("Struct test passed");
 }
 
 int main(void) {
-  test_int();
-  test_struct();
-  return 0;
+   test_int();
+   test_struct();
+   return 0;
 }
--- vec/vector.h
+++ vec/vector.h
@@ -1,55 +1,195 @@
-#ifndef VECTOR_H
-#define VECTOR_H
-#include <stdbool.h>
-#include <stddef.h>
+#ifndef VEC_ITEM_TYPE
+   #error VEC_ITEM_TYPE was not declared
+#endif
 
-typedef struct vec_vector vec_Vector;
+#ifndef VEC_SUFFIX
+   #define VEC_SUFFIX VEC_ITEM_TYPE
+#endif
+
+#define __G(name, type) name##_##type
+#define _G(name, type) __G(name, type)
+#define G(name) _G(name, VEC_SUFFIX)
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct G(vec_vector) {
+   size_t capacity;
+   size_t len;
+   VEC_ITEM_TYPE *vec;
+} G(vec_Vector);
 
 // Initializes a new vector with items of sizeof(T)
-vec_Vector *vec_new(size_t item_size);
+G(vec_Vector) * G(vec_new)(void) {
+   G(vec_Vector) *vector = malloc(sizeof(*vector));
+   if (!vector) {
+      return NULL;
+   }
+
+   *vector = (G(vec_Vector)) {
+      .capacity = 0,
+      .len = 0,
+      .vec = NULL,
+   };
+
+   return vector;
+}
 
 // Resizes vector to fit length
-bool vec_fit(vec_Vector *restrict vec);
+bool G(vec_fit)(G(vec_Vector) *restrict vec) {
+   const size_t power = ceilf(log2f(vec->len + 1));
+   const size_t new_capacity = sizeof(vec->vec[0]) * powf(2, power);
+
+   if (new_capacity < vec->capacity || new_capacity > vec->capacity) {
+      void *tmp = realloc(vec->vec, new_capacity);
+      if (!tmp) {
+         return false;
+      }
+      vec->vec = tmp;
+      vec->capacity = new_capacity;
+   }
+   return true;
+}
 
 // Pushes a value to vector
-bool vec_push(vec_Vector *restrict vec, void *item);
+bool G(vec_push)(G(vec_Vector) *restrict vec, VEC_ITEM_TYPE item) {
+   if (vec->len == 0 && vec->capacity == 0) {
+      vec->vec = malloc(sizeof(vec->vec[0]));
+      if (!vec->vec) {
+         return false;
+      }
+
+      vec->len = 1;
+      vec->capacity = sizeof(vec->vec[0]);
+
+      return true;
+   }
+
+   G(vec_fit)(vec);
+   vec->vec[vec->len] = item;
+   vec->len++;
+
+   return true;
+}
 
 // Returns how many items are in the vector
-size_t vec_len(const vec_Vector *vec);
+size_t G(vec_len)(const G(vec_Vector) * vec) { return vec->len; }
 
 // Returns the total amount of items that can currently be stored
-size_t vec_capacity(const vec_Vector *vec);
+size_t G(vec_capacity)(const G(vec_Vector) * vec) { return vec->capacity; }
 
 // Removes an item from the end of the vector and assigns it to dest
-bool vec_pop(vec_Vector *restrict vec, void *dest);
+bool G(vec_pop)(G(vec_Vector) *restrict vec, VEC_ITEM_TYPE *dest) {
+   if (!vec->vec || vec->len <= 0 || vec->capacity <= 0) {
+      return false;
+   }
+
+   vec->len--;
+   if (dest) {
+      *dest = vec->vec[vec->len];
+   }
+   G(vec_fit)(vec);
+   return true;
+}
 
 // Clears out all the memory used by the vector
-void vec_free(vec_Vector *restrict vec);
+void G(vec_free)(G(vec_Vector) *restrict vec) {
+   free(vec->vec);
+   vec->vec = NULL;
+   free(vec);
+}
 
 // Returns item at index
-bool vec_get(const vec_Vector *vec, const size_t index, void *restrict dest);
+bool G(vec_get)(const G(vec_Vector) * vec, const size_t index, VEC_ITEM_TYPE *restrict dest) {
+   if (!vec->vec || index >= vec->len || vec->capacity == 0) {
+      return false;
+   }
+
+   *dest = vec->vec[index];
+   return true;
+}
 
 // Prints the vector with the specified format specifier
-void vec_printf(const char *restrict fmt, const vec_Vector *vec);
+void G(vec_printf)(const char *restrict fmt, const G(vec_Vector) * vec) {
+   // TODO
+   printf("[");
+   for (size_t i = 0; i < G(vec_len)(vec); i++) {
+      printf(" ");
+      printf(fmt, vec->vec[i]);
+   }
+   printf(" ]\n");
+}
 
 // Inserts a new item at index
-bool vec_insert(vec_Vector *vec, const size_t index, void *item);
+bool G(vec_insert)(G(vec_Vector) * vec, const size_t index, VEC_ITEM_TYPE item) {
+   size_t len = vec->len - 1; // length before growing
+   if (index > len) {
+      return false;
+   }
+
+   G(vec_push)(vec, vec->vec[len]);
+
+   for (size_t i = len; i > index; i--) {
+      vec->vec[i - 1] = vec->vec[i];
+   }
+
+   vec->vec[index] = item;
+   return true;
+}
 
 // Removes an item at index
-bool vec_remove(vec_Vector *restrict vec, const size_t index, void *dest);
+bool G(vec_remove)(G(vec_Vector) *restrict vec, const size_t index, VEC_ITEM_TYPE *dest) {
+   size_t len = vec->len - 1;
+   if (index > len) {
+      return false;
+   }
 
-bool vec_is_empty(const vec_Vector *vec);
+   if (dest) {
+      *dest = vec->vec[index];
+   }
+
+   for (size_t i = index; i < vec->len; i++) {
+      if (i + 1 < vec->len) {
+         vec->vec[i] = vec->vec[i + 1];
+      }
+   }
+
+   return G(vec_pop)(vec, NULL);
+}
+
+bool G(vec_is_empty)(const G(vec_Vector) * vec) {
+   if (vec->len == 0 || vec->capacity == 0 || !vec->vec) {
+      return true;
+   }
+
+   return false;
+}
 
 // Appends vector from src to dest
-bool vec_append(vec_Vector *restrict dest, const vec_Vector *src);
+bool G(vec_append)(G(vec_Vector) *restrict dest, const G(vec_Vector) * src) {
+   for (size_t i = 0; i < src->len; i++) {
+      if (!G(vec_push)(dest, src->vec[i])) {
+         return false;
+      }
+   }
 
-// converts an array into a vector
-//
-// WARNING: this will only work on arrays of type void*
-// if you require sizes bigger than sizeof(void *) you should manually push
-// items into vector
-//
-// This function should be avoided if possible
-vec_Vector *vec_from(void *const array, size_t length, size_t item_size);
+   return true;
+}
 
-#endif
+G(vec_Vector) * G(vec_from)(VEC_ITEM_TYPE *const arr, size_t length, size_t item_size) {
+   G(vec_Vector) *vec = G(vec_new)();
+   for (size_t i = 0; i < length; i++) {
+      if (!G(vec_push)(vec, arr[i])) {
+         G(vec_free)(vec);
+         return NULL;
+      }
+   }
+   return vec;
+}
+
+#undef VEC_ITEM_TYPE
+#undef VEC_SUFFIX
